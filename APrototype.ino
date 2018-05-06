d13255edca2d5e4bbf6d1eccdb83466713867e8f
Daniel Rodríguez <dani1610@hotmail.com>
2018-05-06 19:06:38 +0800

    First aproximation of obstacle avoidance

--- APrototype.ino
+++ APrototype.ino
@@ -28,22 +28,25 @@ void loop() {
   //  Serial.println(analogRead(edgeR));
   //  delay(500);
 
-  if (tsensor.dr > edgeThreshold || tsensor.dl > edgeThreshold) {
+  if (tsensor.dr > edgeThreshold || tsensor.dl > edgeThreshold || ((tsensor.distance <= 25) && (tsensor.distance > 10))) {
     if (tsensor.dr > edgeThreshold) {
       tmotor.turnLeft();
     }
     if (tsensor.dl > edgeThreshold) {
       tmotor.turnRight();
     }
+    if ((tsensor.distance <= 25) && (tsensor.distance > 10)) {
+      tmotor.turnLeft();
+    }
   }
   else {
     tmotor.goStraight();
   }
 
-  //    if ((tsensor.distance <= 25) && (tsensor.distance > 10))
-  //    {
-  //      // if the switch is high, motor will turn on one direction:
-  //      tmotor.turnRight();
-  //    }
+  if ((tsensor.distance <= 25) && (tsensor.distance > 10))
+  {
+    // if the switch is high, motor will turn on one direction:
+    tmotor.turnRight();
+  }
 }
 
